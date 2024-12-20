9ac70efb9330f92cf9bf31725f8f2dcedcf5da58
Minmyatngwe <139677126+Minmyatngwe@users.noreply.github.com>
2024-12-20 20:54:15 +0200

    Update chatbot_update.py

--- chatbot_update.py
+++ chatbot_update.py
@@ -2,9 +2,28 @@
 import tkinter as tk
 from langchain_ollama import OllamaLLM
 from langdetect import detect
-
+from datetime import datetime
+import csv
+import os
 
 model = OllamaLLM(model='llama3')
+#making a dict to store the time ,user_input,and the response from the chatbot
+conversation_dict={"USER_INPUT":"","CHATBOT":"","TIME":datetime.now()}
+
+def track_conversation(user_input):
+    #checking the file has already existed or not
+    file_exit=os.path.exists("conversation.csv")
+    conversation_dict["USER_INPUT"]=user_input
+
+    field_name=["USER_INPUT","CHATBOT","TIME"]
+    with open("conversation.csv","a",newline="") as csvfile:
+        writer=csv.DictWriter(csvfile,fieldnames=field_name)
+        #if the file has not already existed the header will be written
+        if not file_exit:
+            writer.writeheader()
+        writer.writerow(conversation_dict)
+
+
 
 def stream_response(user_input):
     try:
@@ -18,10 +37,15 @@ def stream_response(user_input):
         #seeing the chatbot prompt token by token not to wait unitl it prompts the whole text
 
         for token in response_stream:
+            conversation_dict["CHATBOT"]+=token
             chat_display.insert(tk.END, token)  
             chat_display.update_idletasks()  
             
             chat_display.see(tk.END) 
+        track_conversation(user_input)
+
+
+
 
         chat_display.insert(tk.END, "\n\n")  
         
@@ -69,17 +93,20 @@ chat_display = tk.Text(root, font=("Arial", 12), wrap=tk.WORD)
 
 chat_display.pack(pady=10, padx=10, fill=tk.BOTH, expand=True)
 
-#customize the user_input and send button
-
+#making frame for  user_input and send button
 user_input_frame = tk.Frame(root)
 user_input_frame.pack(fill=tk.X, padx=10, pady=5)
+
+## Text entry widget for user input
 
 user_input_entry = tk.Text(user_input_frame, font=("Arial", 18), height=5, bd=5, relief=tk.GROOVE, wrap=tk.WORD)
 
 user_input_entry.pack(side=tk.LEFT, fill=tk.X, expand=True)
 
+# Button to send the message
 
 send_button = tk.Button(user_input_frame, text="Send", command=send_message,height=5)
+
 
 send_button.pack(side=tk.RIGHT, padx=6)
 
